8443e17628945e5b5fe076dd3ff22351f47ccaa9
muriaraphael <raphael.muria@hotmail.fr>
2017-03-31 15:00:56 +0200

    Update ajouter_admin.php

--- liste-dossier-avec-bdd/ajouter_admin.php
+++ liste-dossier-avec-bdd/ajouter_admin.php
@@ -6,6 +6,11 @@ $email=$_POST['email'];
 $mdp=password_hash($_POST['mdp'], PASSWORD_BCRYPT);
 $sql="INSERT INTO `admin` (`nom`, `prenom`, `email`, `mdp`) VALUES ('".$nom."','".$prenom."','".$email."','".$mdp."')";
 //echo $sql;
+
+include_once("Class_action.php");
+$a = action('Ajout_admin', 'Ajout d\'un compte administrateur.', date("Y-m-d"), 0, $_SESSION['login'], $nom . ' ' . $prenom);
+$a->enregistrer();
+
 mysqli_query($conn, $sql);
 header('location:liste_admins.php');
 ?>
