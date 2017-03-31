632dfcf7384bd8be0d8628878b18fd17eefd3f29
muriaraphael <raphael.muria@hotmail.fr>
2017-03-31 14:59:11 +0200

    Update supprimer_admin.php

--- liste-dossier-avec-bdd/supprimer_admin.php
+++ liste-dossier-avec-bdd/supprimer_admin.php
@@ -1,6 +1,12 @@
 <?php
 include_once("connection.php");
 $id=$_POST['id'];
+
+include_once("Class_action.php");
+$sql="select nom,prenom from `admin` WHERE `id`=".$id;
+$a = action('Supprimer_admin', 'Suppression d\'un compte administrateur.', date("Y-m-d"), 0, $_SESSION['login'], $sql[0] . ' ' . $sql[1]);
+$a->enregistrer();
+
 $sql="DELETE FROM `admin` WHERE `id`=".$id;
 mysqli_query($conn, $sql);
 header('location:liste_admins.php');
